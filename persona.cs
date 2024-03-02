c0f117b743e668f7b8f7a7e1e4ea437f954da140
Michaelsena2523 <senacelularnet@gmail.com>
2024-03-02 00:42:39 +0000

    nueva version de nota de estudiante

--- persona.cs
+++ persona.cs
@@ -1,56 +1,120 @@
+
 using System; 
 
+class CalificacionEstudiante
+{
+    static void Main()
+    {
 
- class persona{
-	 
-	 
-	 static void Main(){
+        double examen1;
+        double examen2;
+        double examen3;
+        double sum = 0;
+        string seguir = "yes";
         
-         string seguirSystema = "yes"; 
-		 double ResultadoPratica = 0;
-    do{		
-	
-		 
-	
-		Console.WriteLine("Ingrese la primera nota");
-        double examen1 = double.Parse(Console.ReadLine());
-   
-        Console.WriteLine("Ingrese la primera nota");  
-        double examen2 = double.Parse(Console.ReadLine());
- 
-        Console.WriteLine("Ingrese la primera nota");  
-        double examen3 = double.Parse(Console.ReadLine()); 
-		
-		
-		for(int i = 1 ; i < 4 ; i++)
-		{
-		 Console.WriteLine("Ingrese la nata de la pratica"+ i ); 
-         double pratica = double.Parse(Console.ReadLine()); 
-         ResultadoPratica += pratica; 		 
-			
-		}
-   
-        double resultadoExamen1 = examen1 * 25 / 100; 
-		double resultadoExamen2 = examen2 * 20 / 100;
-		double resultadoExamen3 = examen3 * 15 / 100;
-		double sumaTotalExamen = resultadoExamen1 + resultadoExamen2 + resultadoExamen3;
-		
-		Console.WriteLine("El resultado es:" +resultadoExamen1);
-		Console.WriteLine("El resultado es:" +resultadoExamen2);
-		Console.WriteLine("El resultado es:" +resultadoExamen3);
-		Console.WriteLine("El total del promedio del Examen es :"+ sumaTotalExamen);
-        Console.WriteLine("EL resultado de la pratica:"+ ResultadoPratica); 		
-    
-	
-	
-	Console.WriteLine("Quieres seguir en systema escriba (yes)"); 
-    seguirSystema = Console.ReadLine(); 
-	
-    Console.Clear();	
 
-	}while(seguirSystema == "yes" ); 
-   
-   
-		 
-	 }
- }
+        do {
+
+            while (true)
+            {
+                Console.WriteLine("Ingrese sus primera calificacion");
+                try {
+                    examen1 = double.Parse(Console.ReadLine());
+                    if (examen1 >= 0 && examen1 <= 100)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero que debe insertar debe ser menor 100 y 0"); 
+                    }
+                }
+                catch (FormatException) 
+                {
+                    Console.WriteLine("Por favor introdusca calificacion validad"); 
+                }
+
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Ingrese sus primera calificacion");
+                try
+                {
+                    examen2 = double.Parse(Console.ReadLine());
+                    if (examen2 >= 0 && examen2 <= 100)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero que debe insertar debe ser menor 100 y 0");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Por favor introdusca calificacion validad");
+                }
+
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Ingrese sus primera calificacion");
+                try
+                {
+                    examen3 = double.Parse(Console.ReadLine());
+                    if (examen3 >= 0 && examen3 <= 100)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero que debe insertar debe ser menor 100 y 0");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Por favor introdusca calificacion validad");
+                }
+
+            }
+
+        for (int i = 1; i < 4 ; i++ )
+        {
+            Console.WriteLine("Ingrese la primera la nota de la pratica" + i); 
+            double pratica = double.Parse(Console.ReadLine());
+            sum += pratica;
+        }
+
+
+
+            double resultado1 = examen1 * 25 / 100;
+            double resultado2 = examen2 * 20 / 100;
+            double resultado3 = examen3 * 25 / 100;
+
+            double resultadoFinal = resultado1 + resultado2 + resultado3;
+            double notaFinalPratica = sum / 100 * 3;
+            double NotaFinalDelEstudiante = resultadoFinal + notaFinalPratica;  
+
+            Console.WriteLine("Su calificacion de primera nota es:" + resultado1);
+            Console.WriteLine("Su csalificacion de primera nota es:" + resultado2);
+            Console.WriteLine("Su calificacion de primera nota es:" + resultado3);
+            Console.WriteLine("Su calificacion final es: " + resultadoFinal);
+            Console.WriteLine("La suma de lo pratica es " + notaFinalPratica);
+            Console.WriteLine("Nota Final del estudiante Aprovado o Reprobado:"+ NotaFinalDelEstudiante);
+
+
+
+            Console.WriteLine("Quieres seguir dentro del systema escriba (yes) caso contrario preciones cual tecla");
+            seguir = Console.ReadLine();
+
+
+            Console.ReadKey();
+            Console.Clear();
+
+        } while(seguir == "yes"); 
+
+
+    }
+}
