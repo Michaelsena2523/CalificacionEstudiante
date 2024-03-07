6df8636a38af25af8428de8f0962f6d28ba305b2
Michaelsena2523 <senacelularnet@gmail.com>
2024-03-07 00:31:08 -0400

    nueva version Calificacion usando herencia y metodo

---
+++ ModificadorAcceso/ModificadorAcceso/Program.cs
@@ -0,0 +1,42 @@
+﻿using System; 
+
+
+class ModificadorAcceso
+{
+
+    int n1, n2, totalsum, totalres;
+
+
+   static void Main() 
+    {
+
+        sum sumar = new sum();
+        sumar.calcularsuma(); 
+
+        Console.ReadKey();
+        Console.Clear();    
+
+    }
+
+
+
+    public class sum
+    {
+
+        public void calcularsuma()
+        {
+
+            ModificadorAcceso objsuma = new ModificadorAcceso();
+
+            Console.Write("ingrese el primer numero:");
+            objsuma.n1 = int.Parse(Console.ReadLine());
+            Console.Write("ingrese el segundo numero:");
+            objsuma.n2 = int.Parse(Console.ReadLine());
+            objsuma.totalsum = objsuma.n1 + objsuma.n2;
+            Console.Write("La suma de los numero es:" +objsuma.totalsum);
+        }
+
+
+    }
+
+}
